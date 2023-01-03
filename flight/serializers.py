405d6fdd1b0ef478711080b426a34e09ac7b85c9
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2023-01-03 10:23:20 +0100

    flight serializer and views added

---
+++ flight/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Flight, Passenger, Reservation
+
+class FlightSerializer(serializers.ModelSerializer) :
+
+    class Meta :
+        model = Flight
+        exclude = []
--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Flight, Passenger, Reservation
+from .serializers import FlightSerializer
+
+class FlightView(ModelViewSet) :
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
