ebca4064e856f77609a751e12bc7189fd5571b09
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2023-01-03 12:33:51 +0100

    queryset filter added

--- flight/serializers.py
+++ flight/serializers.py
@@ -1,8 +1,46 @@
 from rest_framework import serializers
 from .models import Flight, Passenger, Reservation
 
+# GET, PATCH, UPDATE, DELETE, POST
+
 class FlightSerializer(serializers.ModelSerializer) :
 
     class Meta :
         model = Flight
         exclude = []
+
+class PassengerSerializer(serializers.ModelSerializer) :
+
+    class Meta :
+        model = Passenger
+        exclude = []
+
+class ReservationSerializer(serializers.ModelSerializer) :
+    user_id = serializers.IntegerField(required=False)
+    user = serializers.StringRelatedField()
+
+    flight_id = serializers.IntegerField()
+    flight = serializers.StringRelatedField()
+
+    passenger = PassengerSerializer(many = True) # related_name ile baglanti kurduk
+
+    class Meta :
+        model= Reservation
+        exclude = []
+
+    def create(self, validated_data) :
+        print("VALIDATED-DATA :", validated_data)
+
+        passenger_data = validated_data.pop("passenger")
+        print("VALIDATED_DATA-2 :" , validated_data)
+
+        validated_data['user_id'] = self.context["request"].user.id
+        print("VALIDATED_DATA-3 :" , validated_data)
+
+        print("PASSENGER-DATA : ",passenger_data)
+        reservation = Reservation.objects.create(**validated_data)
+        for passenger in passenger_data :
+            pas = Passenger.objects.create(**passenger)
+            reservation.passenger.add(pas)
+            reservation.save()
+            return reservation
--- flight/views.py
+++ flight/views.py
@@ -2,8 +2,30 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Flight, Passenger, Reservation
-from .serializers import FlightSerializer
+from .serializers import FlightSerializer,ReservationSerializer
+
+from rest_framework.permissions import IsAdminUser
+from .permissions import *
 
 class FlightView(ModelViewSet) :
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    #Permissions;
+    permission_classes = [IsStaffOrReadOnly]
+
+
+
+class ReservationView(ModelViewSet) :
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        query_set = super().get_queryset() #Bu orijinal olan queryset functionu. Bir degiskene atadik ve dedik ki ;
+        # Eger istegi yapan kisi staff'ise yani admin ise bütün hepsini döndür. Orijinaldeki gibi yani.. Ama eger degilse istegi atana user ile db'de eslesen user varsa onun bilgilerini getir.
+        #YANI USER SADECE KENDI OLUSTURDUGU RESERVATIONLARI GÖREBILIR. EGER ADMIN DEGISE
+        if self.request.user.is_staff :
+            return query_set
+        else :
+            return query_set.filter(user=self.request.user)
+
+        
